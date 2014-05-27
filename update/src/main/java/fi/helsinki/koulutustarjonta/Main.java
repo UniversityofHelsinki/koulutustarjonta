bc06cdcc70b9f3895906c63d5100f5eecac5af52
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2014-05-27 16:30:10 +0300

    KOTA-29: Added a gradle project skeleton.

---
+++ update/src/main/java/fi/helsinki/koulutustarjonta/Main.java
@@ -0,0 +1,16 @@
+package fi.helsinki.koulutustarjonta;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("main run");
+    }
+
+}
