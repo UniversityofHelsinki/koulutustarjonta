8c2c47a2266780f8f71af44fa2afc22ead2cb5b4
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2014-05-28 08:33:05 +0300

    KOTA-29: Added a test main class.

---
+++ update/src/test/java/fi/helsinki/koulutustarjonta/MainTest.java
@@ -0,0 +1,16 @@
+package fi.helsinki.koulutustarjonta;
+
+import org.junit.Test;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        String[] args = new String[0];
+        Main.main(args);
+        // nothing happens, excellent
+    }
+}
