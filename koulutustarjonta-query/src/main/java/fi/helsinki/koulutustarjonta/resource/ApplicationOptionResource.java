ccb3d441ebe8cd6868c79e6056b6284de0157438
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2014-07-29 15:52:38 +0300

    KOTA-85: implemented the application option root resource for fetching all application options

--- koulutustarjonta-query/src/main/java/fi/helsinki/koulutustarjonta/resource/ApplicationOptionResource.java
+++ koulutustarjonta-query/src/main/java/fi/helsinki/koulutustarjonta/resource/ApplicationOptionResource.java
@@ -1,13 +1,16 @@
 package fi.helsinki.koulutustarjonta.resource;
 
 import fi.helsinki.koulutustarjonta.dao.ApplicationOptionDAO;
-import fi.helsinki.koulutustarjonta.domain.ApplicationOption;
+import fi.helsinki.koulutustarjonta.dto.ApplicationOptionDTO;
+import org.modelmapper.ModelMapper;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Hannu Lyytikainen
@@ -17,22 +20,24 @@ import javax.ws.rs.core.MediaType;
 public class ApplicationOptionResource {
 
     private final ApplicationOptionDAO applicationOptionDAO;
+    private final ModelMapper modelMapper;
 
     public ApplicationOptionResource(ApplicationOptionDAO applicationOptionDAO) {
         this.applicationOptionDAO = applicationOptionDAO;
+        this.modelMapper = new ModelMapper();
     }
 
     @GET
-    public String getApplicationOptions() {
-
-        return "ok";
+    public List<ApplicationOptionDTO> getApplicationOptions() {
+        return applicationOptionDAO.findAll().parallelStream()
+                .map(ao -> modelMapper.map(ao, ApplicationOptionDTO.class))
+                .collect(Collectors.toList());
     }
 
     @GET
     @Path("/{oid}")
-    public String getApplicationOption(@PathParam("oid") String oid) {
-        ApplicationOption applicationOption = applicationOptionDAO.findByOid(oid);
-        return "ok";
+    public ApplicationOptionDTO getApplicationOption(@PathParam("oid") String oid) {
+        return modelMapper.map(applicationOptionDAO.findByOid(oid), ApplicationOptionDTO.class);
     }
 
 }
