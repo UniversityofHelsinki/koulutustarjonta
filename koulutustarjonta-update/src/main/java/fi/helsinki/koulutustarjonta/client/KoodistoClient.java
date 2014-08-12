819508965225cd0d15287013d6210985ebbab190
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2014-08-12 10:40:35 +0300

    Added code version support to koodisto client.

--- koulutustarjonta-update/src/main/java/fi/helsinki/koulutustarjonta/client/KoodistoClient.java
+++ koulutustarjonta-update/src/main/java/fi/helsinki/koulutustarjonta/client/KoodistoClient.java
@@ -25,17 +25,51 @@ public class KoodistoClient {
         this.codeConverter = new CodeConverter();
     }
 
-    public Code getCode(String codeUri) {
+    public Code getCode(String codeUriAndVersion) {
+        String codeUri = parseCodeUri(codeUriAndVersion);
+        String version = parseVersion(codeUriAndVersion);
         LOG.debug(String.format("Retrieving code with uri %s", codeUri));
         String path = String.format("%s/%s/%s",
                 parseCodeGroup(codeUri), CODE_PATH, codeUri);
+        WebResource resource = codeResource.path(path);
 
-        JsonNode json = codeResource.path(path).get(new GenericType<JsonNode>() {});
+        if (version != null) {
+            resource = resource.queryParam("koodistoVersio", version);
+        }
+        JsonNode json = resource.get(new GenericType<JsonNode>() {});
         return codeConverter.convert(json);
     }
 
     /**
-     * Parse code group (koodisto) name from code uri.
+     * Parse code uri form code uri and version string,
+     * such as posti_00180#1
+     *
+     * @param uriAndVersion
+     * @return
+     */
+    private String parseCodeUri(String uriAndVersion) {
+        return uriAndVersion.split("#")[0];
+    }
+
+    /**
+     * Parse code version form code uri and version string,
+     * such as posti_00180#1
+     *
+     * @param uriAndVersion
+     * @return
+     */
+    private String parseVersion(String uriAndVersion) {
+        String[] parts = uriAndVersion.split("#");
+        if (parts.length > 1) {
+            return parts[1];
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * Parse code group (koodisto) name from code uri, for example posti_00180.
      * @param codeUri code uri
      * @return code group name
      */
