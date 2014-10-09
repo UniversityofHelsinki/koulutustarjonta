e6f6c6999c02f9bf4d8c5bc4e2ed636ad0088f78
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2014-10-09 12:52:13 +0300

    Improved logging.

--- koulutustarjonta-common/src/main/java/fi/helsinki/koulutustarjonta/dao/LearningOpportunityDAO.java
+++ koulutustarjonta-common/src/main/java/fi/helsinki/koulutustarjonta/dao/LearningOpportunityDAO.java
@@ -3,6 +3,8 @@ package fi.helsinki.koulutustarjonta.dao;
 import fi.helsinki.koulutustarjonta.dao.jdbi.LearningOpportunityJDBI;
 import fi.helsinki.koulutustarjonta.dao.mapper.LearningOpportunityObjectGraphBuilder;
 import fi.helsinki.koulutustarjonta.domain.LearningOpportunity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,6 +14,8 @@ import java.util.stream.Collectors;
  */
 public class LearningOpportunityDAO {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LearningOpportunityDAO.class);
+
     final LearningOpportunityJDBI jdbi;
 
     public LearningOpportunityDAO(LearningOpportunityJDBI jdbi) {
@@ -19,6 +23,7 @@ public class LearningOpportunityDAO {
     }
 
     public void save(LearningOpportunity learningOpportunity) {
+        LOG.debug(String.format("Saving learning opportunity %s", learningOpportunity.getOid()));
         jdbi.upsert(learningOpportunity);
         jdbi.insertTeachingLanguages(learningOpportunity.getTeachingLanguages());
         jdbi.removeTeachingLanguagesFromLearningOpportunity(learningOpportunity.getOid());
@@ -26,8 +31,10 @@ public class LearningOpportunityDAO {
                 learningOpportunity.getTeachingLanguages()
                         .stream()
                         .map(x -> x.getLang())
-                        .collect(Collectors.toList()));
-        if (learningOpportunity.getApplicationOptions() != null) {
+                        .collect(Collectors.toList())
+        );
+        if (learningOpportunity.getApplicationOptions() != null && !learningOpportunity.getApplicationOptions().isEmpty()) {
+            LOG.debug(String.format("Adding application options to learning opportunity %s", learningOpportunity.getApplicationOptions().toString()));
             jdbi.addApplicationOptions(learningOpportunity.getOid(), learningOpportunity.getApplicationOptions());
             jdbi.removeDeletedApplicationOptions(learningOpportunity.getOid(), learningOpportunity.getApplicationOptions());
         }
